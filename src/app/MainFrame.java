fc8e3e694a76d777cf48dca67c206da84408caa5
Blokoy <j97873661@gmail.com>
2026-03-25 20:50:01 +0800

    small change

--- src/app/MainFrame.java
+++ src/app/MainFrame.java
@@ -54,7 +54,7 @@ public class MainFrame extends JFrame {
     tabbedPane.addTab("Add Student", new AddStudentPanel());
     tabbedPane.addTab("View Students", new ViewStudentsPanel());
     tabbedPane.addTab("Search Student", new SearchStudentPanel());
-//    tabbedPane.addTab("Edit / Delete", new EditStudentPanel());
+    tabbedPane.addTab("Edit / Delete", new EditStudentPanel());
 //    tabbedPane.addTab("File", new FilePanel());
 //    tabbedPane.addTab("Exit / Help", new ExitHelpPanel());
 //    tabbedPane.addTab("About", new AboutPanel());
