ab7667f193b30d1ad9cf1fb14a01a61ac6bd5bb2
Blokoy <j97873661@gmail.com>
2026-03-25 20:45:29 +0800

    Theme update and menu bar

--- src/app/MainFrame.java
+++ src/app/MainFrame.java
@@ -8,9 +8,9 @@ import java.awt.*;
 
 /**
  * Main application frame. Assembles all panels into a tabbed layout.
- * 
+ *
  * ASSIGNED TO: Student 2 (Main Frame / App Shell Owner)
- * 
+ *
  * TODO for Student 2:
  * - Customize the look and feel (colors, fonts, window size)
  * - Add a menu bar if desired (File > Exit, Help > About)
@@ -23,11 +23,29 @@ public class MainFrame extends JFrame {
       Image image = Toolkit.getDefaultToolkit().getImage("appLOGO/logo.png");
       ImageIcon icon = new ImageIcon();
       setIconImage(icon.getImage());
-    setTitle("Student Management System");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(800, 600);
-    setMinimumSize(new Dimension(700, 500));
-    setLocationRelativeTo(null); // Center on screen
+      setTitle("Student Management System");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setSize(800, 600);
+      setMinimumSize(new Dimension(700, 500));
+      setLocationRelativeTo(null); // Center on screen
+
+      JMenuBar menuBar = new JMenuBar();
+
+      JMenu fMennu = new JMenu("File");
+      JMenuItem exit = new JMenuItem("Exit");
+      exit.addActionListener(e -> System.exit(0));
+      fMennu.add(exit);
+
+      JMenu hMeu = new JMenu("Help");
+      JMenuItem aboutItem = new JMenuItem("About");
+      aboutItem.addActionListener(e -> JOptionPane.showMessageDialog(this,
+              "Student Management System\nMembers:\nCastillo\nGonzalez\nInocencio\nLopez",
+              "About", JOptionPane.INFORMATION_MESSAGE));
+      hMeu.add(aboutItem);
+
+      menuBar.add(fMennu);
+      menuBar.add(hMeu);
+      setJMenuBar(menuBar);
 
     // --- Tabbed Pane: each tab is owned by a different student ---
     JTabbedPane tabbedPane = new JTabbedPane();
@@ -36,10 +54,10 @@ public class MainFrame extends JFrame {
     tabbedPane.addTab("Add Student", new AddStudentPanel());
     tabbedPane.addTab("View Students", new ViewStudentsPanel());
     tabbedPane.addTab("Search Student", new SearchStudentPanel());
-    tabbedPane.addTab("Edit / Delete", new EditStudentPanel());
-    tabbedPane.addTab("File", new FilePanel());
-    tabbedPane.addTab("Exit / Help", new ExitHelpPanel());
-    tabbedPane.addTab("About", new AboutPanel());
+//    tabbedPane.addTab("Edit / Delete", new EditStudentPanel());
+//    tabbedPane.addTab("File", new FilePanel());
+//    tabbedPane.addTab("Exit / Help", new ExitHelpPanel());
+//    tabbedPane.addTab("About", new AboutPanel());
 
     add(tabbedPane, BorderLayout.CENTER);
   }
