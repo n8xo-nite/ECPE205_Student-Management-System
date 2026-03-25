70bd158cee27fcef3dadb4b15ad1eda3e5bae1d1
Blokoy <j97873661@gmail.com>
2026-03-25 20:56:23 +0800

    STUDENT 2 Logo updated

--- src/app/MainFrame.java
+++ src/app/MainFrame.java
@@ -20,9 +20,6 @@ import java.awt.*;
 public class MainFrame extends JFrame {
 
   public MainFrame() {
-      Image image = Toolkit.getDefaultToolkit().getImage("appLOGO/logo.png");
-      ImageIcon icon = new ImageIcon();
-      setIconImage(icon.getImage());
       setTitle("Student Management System");
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setSize(800, 600);
@@ -60,6 +57,15 @@ public class MainFrame extends JFrame {
 //    tabbedPane.addTab("About", new AboutPanel());
 
     add(tabbedPane, BorderLayout.CENTER);
+
+    try{
+        ImageIcon icon = new ImageIcon("appLOGO/logo.png");
+            setIconImage(icon.getImage());
+      } catch (Exception e) {
+        System.err.println("Error loading application icon.");
+    }
+
+
   }
 
   public static void main(String[] args) {
